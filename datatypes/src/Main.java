d7543ad2ddc21174ac1137c54d7d31c70938c593
Furkan394 <furkanncakiroglu@gmail.com>
2022-09-15 19:34:08 +0300

    Homework 1

---
+++ datatypes/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi = 12;
+        double sayi2 = 12.5;
+        String sehir = "Ankara";
+        char karakter = 'A';
+        boolean dogruMu = false;
+    }
+}
